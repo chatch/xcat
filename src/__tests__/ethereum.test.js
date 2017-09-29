adfa5deb381b4ffad7053a4f610a20fe18bf37b8
Chris Hatch <hatch@tuta.io>
2017-09-29 14:21:33 +0700

    use truffle-contract instead of web3-eth (1.0.0-beta.20) as the client to the smart contract

--- src/__tests__/ethereum.test.js
+++ src/__tests__/ethereum.test.js
@@ -1,25 +1,49 @@
 import expect from 'expect'
+import Promise from 'bluebird'
+
 import Ethereum from '../ethereum'
+import {random32} from '../utils'
 import HTLC from '../contracts/HashedTimelock.json'
 
 const RPC_ADDR = 'http://localhost:8545'
 const NETWORK = 'ropsten'
 
 describe('ethereum', () => {
-  it('creates a new instance', () => {
-    const eth = new Ethereum(RPC_ADDR, NETWORK, HTLC)
-    expect(eth.web3Eth.currentProvider.host).toEqual(RPC_ADDR)
-    expect(eth.htlc.options.jsonInterface).toEqual(HTLC.abi)
-    expect(eth.htlc.options.address.toLowerCase()).toEqual(
-      HTLC.deployed.ropsten.toLowerCase()
-    )
+  describe('constructor', () => {
+    it('creates a new instance', () => {
+      const eth = new Ethereum(RPC_ADDR, NETWORK, HTLC)
+      expect(eth.web3Eth.currentProvider.host).toEqual(RPC_ADDR)
+      expect(eth.htlc.abi).toEqual(HTLC.abi)
+      expect(eth.htlc.address.toLowerCase()).toEqual(
+        HTLC.deployed.ropsten.toLowerCase()
+      )
+    })
+
+    it('throws if no deployed contract address found for given network', () => {
+      const expectedError =
+        'No contract deployment address found for HashedTimelock on network [privatenet].'
+      expect(() => new Ethereum(RPC_ADDR, 'privatenet', HTLC)).toThrowError(
+        expectedError
+      )
+    })
   })
 
-  it('throws if no deployed contract address found for given network', () => {
-    const expectedError =
-      'No contract deployment address found for HashedTimelock on network [privatenet].'
-    expect(() => new Ethereum(RPC_ADDR, 'privatenet', HTLC)).toThrowError(
-      expectedError
-    )
+  describe('createHashedTimelockContract', () => {
+    it('submits new contract tx and returns contract id', done => {
+      const eth = new Ethereum(RPC_ADDR, NETWORK, HTLC)
+
+      const contractId = random32().toString('hex')
+      const txReceipt = {receipt: {logs: [{data: contractId}]}}
+      eth.htlc.newContract = jest.fn(() => Promise.resolve(txReceipt))
+
+      const hashX = random32().toString('hex')
+      const amount = 0.01
+      eth
+        .createHashedTimelockContract(hashX, 0x0, 0x0, amount, 0)
+        .then(newContractId => {
+          expect(newContractId).toEqual(contractId)
+          done()
+        })
+    })
   })
 })
--- src/ethereum.js
+++ src/ethereum.js
@@ -1,6 +1,7 @@
 import Web3Eth from 'web3-eth'
 import Web3Utils from 'web3-utils'
 import has from 'lodash/has'
+import truffleContract from 'truffle-contract'
 
 const txRetVal = txReceipt => txReceipt.receipt.logs[0].data
 const ethToWei = eth => Web3Utils.toWei(eth, 'ether')
@@ -20,21 +21,22 @@ const contractArrToObj = c => {
 class Ethereum {
   /**
    * Setup web3 and a handle to the HashedTimelock contract
+   *
    * @param rpcAddr Address of Ethereum client RPC
    * @param network Ethereum network to connect to (eg. 'ropsten', 'mainnet')
-   * @param htlcContract Object containing HashedTimelock contract details:
-   *          abi: contract interface
-   *          deployed: { ropsten: '0xabc...', 'mainnet: 0x123...', etc.
+   * @param htlcContractObj Truffle contract generated object containing
+   *          details of the contract including the abi. Additionally it should
+   *          contain deployed addresses for each 'network' as:
+   *            deployed: { ropsten: '0xabc...', 'mainnet: 0x123...', etc.
    */
-  constructor(rpcAddr, network, htlcContract) {
-    if (!has(htlcContract.deployed, network))
+  constructor(rpcAddr, network, htlcContractObj) {
+    if (!has(htlcContractObj.deployed, network))
       throw new Error(
         `No contract deployment address found for HashedTimelock on ` +
           `network [${network}].`
       )
 
-    const htlcAddr = htlcContract.deployed[network]
-
+    const htlcAddr = htlcContractObj.deployed[network]
     if (!Web3Utils.isAddress(htlcAddr))
       throw new Error(
         `HashedTimelock deployment address [${htlcAddr}] ` +
@@ -42,78 +44,54 @@ class Ethereum {
       )
 
     this.web3Eth = new Web3Eth(rpcAddr)
-    this.htlc = new this.web3Eth.Contract(htlcContract.abi, htlcAddr)
+
+    const HTLC = truffleContract(htlcContractObj)
+    HTLC.setProvider(this.web3Eth.currentProvider)
+    this.htlc = HTLC.at(htlcAddr)
   }
 
   /**
    * Set up a new hashed timelock contract.
+   *
+   * @return Promise with contractId of the new HTLC
    */
   createHashedTimelockContract(hashX, sellerAddr, buyerAddr, amount, locktime) {
     const ethHashX = hashX.startsWith('0x') ? hashX : '0x' + hashX
-    // return this.htlc.methods
-    //   .newContract(buyerAddr, ethHashX, locktime)
-    //   .send({
-    //     from: sellerAddr,
-    //     value: ethToWei(amount),
-    //   })
-    //   .on('error', error => onErrorCallback(error))
-    //   .on('transactionHash', txHash =>
-    //     console.info(`tx hash[newContract]: ${txHash}`)
-    //   )
-    //   .on('receipt', receipt => onSuccessCallback(txRetVal(receipt)))
-    return this.htlc.methods
-      .newContract(buyerAddr, ethHashX, locktime)
-      .send({
+    const amountWei = ethToWei(amount)
+    return this.htlc
+      .newContract(buyerAddr, ethHashX, locktime, {
         from: sellerAddr,
-        value: ethToWei(amount),
+        value: amountWei,
       })
-      .on('transactionHash', txHash =>
-        console.info(`tx hash[newContract]: ${txHash}`)
-      )
+      .then(txReceipt => txRetVal(txReceipt)) // contractId)
   }
 
   /**
    * Buyer withdraws the funds revealing the preimage for hash(x)
+   *
+   * @return Promise with tx receipt
    */
-  buyerWithdraw(
-    contractId,
-    preimage,
-    buyerAddr,
-    onSuccessCallback,
-    onErrorCallback
-  ) {
-    this.htlc.methods
-      .withdraw(contractId, preimage)
-      .send({
-        from: buyerAddr,
-      })
-      .on('error', error => onErrorCallback(error))
-      .on('transactionHash', txHash =>
-        console.info(`tx hash[withdraw]: ${txHash}`)
-      )
-      .on('receipt', receipt => onSuccessCallback(receipt))
+  buyerWithdraw(contractId, preimage, buyerAddr) {
+    return this.htlc.withdraw(contractId, preimage, {from: buyerAddr})
   }
 
   /**
    * Seller claims refund assuming the transfer did not complete.
    * Seller can not call this before the timelock has expired.
+   *
+   * @return Promise with tx receipt
    */
-  sellerRefund(contractId, sellerAddr, onSuccessCallback, onErrorCallback) {
-    this.htlc.methods
-      .refund(contractId)
-      .send({from: sellerAddr})
-      .on('error', error => onErrorCallback(error))
-      .on('transactionHash', txHash =>
-        console.info(`tx hash[refund]: ${txHash}`)
-      )
-      .on('receipt', receipt => onSuccessCallback(receipt))
+  sellerRefund(contractId, sellerAddr) {
+    return this.htlc.refund(contractId, {from: sellerAddr})
   }
 
+  /**
+   * Fetch HTLC contract details
+   *
+   * @return Promise with contract details - see contractArrToObj for prop names
+   */
   getContract(contractId) {
-    return this.htlc.methods
-      .getContract(contractId)
-      .call()
-      .then(c => contractArrToObj(c))
+    return this.htlc.getContract.call(contractId).then(c => contractArrToObj(c))
   }
 }
 
